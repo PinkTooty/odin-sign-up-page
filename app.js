58b55a16d669e243c575e66330adf7f3e7ca5c7d
PinkToot <huynh.daithanh1@gmail.com>
2022-06-25 18:55:47 -0400

    Add password confirmation and add borders to form inputs

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const submit = document.querySelector('button')
-let pwd1;
-let pwd2;
-const passwords = document.querySelectorAll('.passwords')
-
-submit.addEventListener('click', () => {
-    pwd1 = document.querySelector('#pwd').value
-    pwd2 = document.querySelector('#pwd-confirm').value
-    if (pwd1 !== pwd2) {
-        passwords.forEach((password) => {
-            password.classList.add('error')
-        })
-    } else if (pwd1 === pwd2) {
-        return
+const check = () => {
+    const password = document.querySelector('#password');
+    const confirm = document.querySelector('#confirm-password');
+    if (confirm.value === password.value) {
+        confirm.setCustomValidity('');
+    } else {
+        confirm.setCustomValidity('Passwords do not match');
     }
-})
+}
